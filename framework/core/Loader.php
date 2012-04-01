8ebe4c67563526ec1410f7af87ed21b0e8271e4f
illusion9 <alex@ivkoni.com>
2012-04-01 23:54:33 +0300

    Start of Router, Loader and ErrorHandler

--- framework/core/Loader.php
+++ framework/core/Loader.php
@@ -1,15 +1,56 @@
 <?php if(!defined("FRAMEWORK_LOADED")) exit('File access denied!');
 
 class Loader {
-	public function __construct() {
-		
-		
-		
+
+	public function load_controller($name) {
+		$this->load_class("controllers/" + $name);
 	}
-	
-	public function loadController($class) {
-		
-		
-		
+
+	public function load_class($class, $params = null) {
+		$class = str_replace(EXT, '', trim($class, '/'));
+
+		$subdir = '';
+		if (($last_slash = strrpos($class, '/')) !== FALSE) {
+			$subdir = substr($class, 0, $last_slash + 1);
+			$class = substr($class, $last_slash + 1);
+		}
+
+		foreach (array(ucfirst($class), strtolower($class)) as $class) {
+			$subclass = APPPATH.'components/'.$subdir.$class.EXT;
+
+			if (file_exists($subclass)) {
+				include_once($subclass);
+
+				return $this->really_load_class($class, "", $params, $object_name);
+			}
+
+		}
+
+		if ($subdir == '') {
+			$path = strtolower($class).'/'.$class;
+			return $this->really_load_class($path, $params);
+		}
+	}
+
+
+	public function really_load_class($class, $prefix = '', $config = FALSE, $object_name = NULL) {
+		if ( ! class_exists($class)) {
+			die(show_error("Non-existent class: ".$class));
+		}
+
+		$class = strtolower($class);
+
+		if (is_null($object_name)) {
+			$classvar = $class;
+		} else {
+			$classvar = $object_name;
+		}
+
+		$ss =& get_instance();
+		if ($config !== NULL) {
+			$ss->$classvar = new $class($config);
+		} else {
+			$ss->$classvar = new $class;
+		}
 	}
 }
--- framework/core/SummerSnow.php
+++ framework/core/SummerSnow.php
@@ -1,27 +1,43 @@
 <?php if(!defined("FRAMEWORK_LOADED")) exit('File access denied!');
 
+require_once("ErrorHandler.php");
 require_once("Loader.php");
 require_once("Router.php");
 
 class SummerSnow {
 
-	public static $router;
+	public static $instance;
+	public $router;
 
 	public function __construct() {
-		getRouter();
-		loadController(self::$router->getClassName());
+		$this->getRouter();
+		$this->loadController($this->router->get_class_name());
+	}
+
+	public static function getInstance() {
+		if (!self::$instance) {
+			self::$instance = new SummerSnow();
+		}
+
+		return self::$instance;
 	}
 
 	private function getRouter() {
-		$url = $_GET['url'];
-		self::$router = new Router($url);
+		$url = isset($_GET['url']) ? $_GET['url'] : $_SERVER['REQUEST_URI'];
+		$this->router = new Router($url);
 	}
 
 	private function loadController($class) {
 		$loader = new Loader();
-		$loader->loadController($class);
+		echo $class ."<br/>";
+		echo $this->router->get_method_name();
+		$loader->load_controller($class);
 	}
 
+}
+
+function get_instance() {
+	return SummerSnow::getInstance();
 }
 
 // http://www.talkphp.com/advanced-php-programming/1304-how-use-singleton-design-pattern.html
